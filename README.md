4ad73ca73528d10963048eb26bbd362453fd6ad7
Dattasai75 <sdattasai75@gmail.com>
2024-09-14 20:21:33 +0530

    Added Get your qutoe

--- README.md
+++
@@ -1,2 +0,0 @@
-Carlyle
-# Carlyle_Play
--- playwright.config.js
+++ playwright.config.js
@@ -20,9 +20,9 @@ module.exports = defineConfig({
   // retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
-  workers: 1,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+ //reporter: 'html',
   timeout: 2 * 120000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
@@ -46,15 +46,15 @@ module.exports = defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
